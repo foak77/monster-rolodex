d66afb8a4d81df0532c70a6b1a2b79af9be4efad
foak77 <emailtofred@gmail.com>
2020-09-30 21:01:40 -0400

    Monster Rolodex React exercise

---
+++ src/components/card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../card/Card.css";
+
+function Card(props) {
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      ></img>
+      <h1>{props.monster.name}</h1>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+}
+
+export default Card;
